c5046780a5d074466b3915f093a123b986f194fe
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2024-07-16 11:48:11 -0300

    Término do projeto

--- MyNotes.cs
+++ MyNotes.cs
@@ -28,13 +28,17 @@ namespace MyNotes
         }
         static void AdicionarNota()
         {
+            // criando uma nova nota (1/2)
             Console.Clear();
             Console.Write("Digite o título da nota: "); string titulo = Console.ReadLine();
             Console.WriteLine("=========================");
             Console.WriteLine("Abaixo, digite o conteúdo da nota"); string conteudo = Console.ReadLine();
 
+            // class Notas
             Notas nota = new Notas(titulo, conteudo);
             notes.Add(nota);
+
+            // criando uma nova nota (2/2)
             Console.WriteLine("Nota adicionada com sucesso.");
             Console.WriteLine("");
             Console.WriteLine("Deseja visualizar a nota? (S/N)"); char res = char.Parse(Console.ReadLine());
@@ -45,6 +49,8 @@ namespace MyNotes
         static void VisualizarNota()
         {
          Console.Clear();
+
+         // p/ procurar a nota em todas as notas presentes
          foreach(Notas nota in notes)
             {
                 Console.WriteLine($"Título da nota: {nota.Titulo}");
@@ -52,18 +58,46 @@ namespace MyNotes
                 Console.WriteLine("=======");
                 Console.WriteLine("");
                 
-                Console.WriteLine("Deseja criar uma nova nota?"); char res = char.Parse(Console.ReadLine());
-                if(res == 'S') AdicionarNota();
+                Console.WriteLine("Deseja editar nota? (S/N)"); char res = char.Parse(Console.ReadLine());
+                if(res == 'S') EditarNota();
                 else System.Environment.Exit(0);
             }
         }
         static void EditarNota()
         {
-            
+            Console.Clear();
+            Console.Write("Digite o título da nota que deseja editar: "); string titulo = Console.ReadLine();
+
+            Notas nota = notes.Find(n => n.Titulo == titulo);
+
+            if(nota != null)
+            {
+                Console.Write("Digite o novo título para a nota: "); string novoTitulo = Console.ReadLine();
+                nota.Titulo = novoTitulo;
+                Console.WriteLine("Digite o novo conteúdo da nota abaixo"); string novoConteudo = Console.ReadLine();
+                nota.Conteudo = novoConteudo;
+
+                Console.WriteLine("Nota editada com sucesso.");
+            }
+            else Console.WriteLine("Nota não encontrada");
+            Console.WriteLine("");
+            Console.WriteLine("Pré-Visualização");
+            Console.WriteLine($"Novo título: {nota.Titulo}");
+            Console.WriteLine($"Novo conteúdo: {nota.Conteudo}");
         }
         static void ExcluirNota()
         {
+            Console.Clear();
+            Console.Write("Digite o título da nota que deseja excluir: "); string titulo = Console.ReadLine();
 
+            Notas nota = notes.Find(n => n.Titulo == titulo);
+
+            if (nota != null)
+            {
+                notes.Remove(nota);
+                Console.WriteLine("Nota excluída com sucesso.");
+            }
+            else Console.WriteLine("Nota não encontrada");
         }
     }
     class Notas
